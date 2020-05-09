af71a528d9d77f47431d1b1428f86c62b3134874
aristideriou <aristide.riou@gmail.com>
2020-05-09 17:28:01 +0200

    basic adblocker test page

---
+++ lab_adblocking/ads.js
@@ -0,0 +1,7 @@
+window.AdsRead = true;
+
+console.log('foo');
+
+let foo = 'bar';
+
+document.getElementById('pagetitle').insertAdjacentHTML('afterend','<div id=\'adBanner\'>Div insérée dynamiquement</div>');
