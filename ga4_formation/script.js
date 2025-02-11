794338f027a2c4e51feef095384ee556cf53cdc9
aristideriou <aristide.riou@gmail.com>
2025-02-11 21:58:42 +0100

    update DL GA4

--- ga4_formation/script.js
+++ ga4_formation/script.js
@@ -3,7 +3,7 @@ const newsletterButton = document.getElementById("newsletter");
 newsletterButton.addEventListener("click", () => {
     newsletterButton.innerHTML = "Merci, vous êtes maintenant abonné";
     window.dataLayer = window.dataLayer || [];
-
+    
     dataLayer.push({
         event: "newsletterSubscription",
         clientType: ["Prospect", "Client", "Nouveau Client"][
@@ -22,14 +22,74 @@ const pushButton = document.getElementById("push");
 pushButton.addEventListener("click", () => {
     const state = { page_id: 1, user_id: 5 };
     const url = Array.from({ length: Math.floor(Math.random() * 6) + 5 }, () =>
-    Math.random().toString(36).charAt(2)
-    ).join("");
-    
-    history.pushState(state, "", url);
+        Math.random().toString(36).charAt(2)
+).join("");
+
+history.pushState(state, "", url);
 });
 
 
-const purchase = document.getElementById("purchase");
+const purchaseFlat = document.getElementById("purchase");
+purchaseFlat.addEventListener("click", () => {
+    purchaseFlat.innerHTML =  "Votre achat a été validé et votre carte bleue débitée";
+    window.dataLayer = window.dataLayer || [];
+    dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
+dataLayer.push({
+  event: "purchase",
+  userEmail : ["pierredupont@gmail.com", "cmailledubois@yahoo.fr", "claude.martin@caramail.com"][Math.floor(Math.random() * 3)],
+  userFirstName : ["Pierre", "Camille", "Claude"][Math.floor(Math.random() * 3)] ,
+  userLastName : ["Dupont", "Dubois", "Martin"][Math.floor(Math.random() * 3)] ,
+  userCity : ["Paris", "Rennes", "Lille"][Math.floor(Math.random() * 3)],
+  userPhone : ["0611111111", "0600000000", "0899999999"][Math.floor(Math.random() * 3)],
+  userZip : ["75001", "35000", "85000"][Math.floor(Math.random() * 3)],
+  userBirthDate : ["19880221", "19900312", "19920101"][Math.floor(Math.random() * 3)] ,
+  ecommerce: {
+    transaction_id: "T_12345",
+    value: 72.05,
+    tax: 3.60,
+    shipping: 5.99,
+    currency: "USD",
+    coupon: "SUMMER_SALE",
+    items: [
+    {
+      item_id: "SKU_12345",
+      item_name: "Stan and Friends Tee",
+      affiliation: "Google Merchandise Store",
+      coupon: "SUMMER_FUN",
+      discount: 2.22,
+      index: 0,
+      item_brand: "Google",
+      item_category: "Apparel",
+      item_list_id: "related_products",
+      item_list_name: "Related Products",
+      item_variant: "green",
+      location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
+      price: 10.01,
+      quantity: 3
+    },
+    {
+      item_id: "SKU_12346",
+      item_name: "Google Grey Women's Tee",
+      affiliation: "Google Merchandise Store",
+      coupon: "SUMMER_FUN",
+      discount: 3.33,
+      index: 1,
+      item_brand: "Google",
+      item_category: "Apparel",
+      item_list_id: "related_products",
+      item_list_name: "Related Products",
+      item_variant: "gray",
+      location_id: "ChIJIQBpAG2ahYAR_6128GcTUEo",
+      price: 21.01,
+      promotion_id: "P_12345",
+      promotion_name: "Summer Sale",
+      quantity: 2
+    }]
+  }
+});
+});
+
+const purchase = document.getElementById("purchase-flat");
 purchase.addEventListener("click", () => {
     purchase.innerHTML =  "Votre achat a été validé et votre carte bleue débitée";
     window.dataLayer = window.dataLayer || [];
@@ -46,78 +106,78 @@ purchase.addEventListener("click", () => {
         transactionId: ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(
             /[018]/g,
             (c) =>
-            (
+                (
                 c ^
                 (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+            ).toString(16)
+        ),
+        shipping: Math.round((Math.random() * (100 - 10) + 10) * 100) / 100,
+        currency: ["EUR", "USD", "CHF"][Math.floor(Math.random() * 3)],
+        products: [
+            {
+                SKU: ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+                    (
+                    c ^
+                    (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
                 ).toString(16)
-                ),
-                shipping: Math.round((Math.random() * (100 - 10) + 10) * 100) / 100,
-                currency: ["EUR", "USD", "CHF"][Math.floor(Math.random() * 3)],
-                products: [
-                    {
-                        SKU: ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-                        (
-                            c ^
-                            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-                            ).toString(16)
-                            ),
-                            name: ["GA4", "GTM", "BigQuery"][Math.floor(Math.random() * 3)],
-                            category: "Formations",
-                            price: Math.floor(Math.random() * (5000 - 1000 + 1)) + 300,
-                            duration: Math.floor(Math.random() * (5 - 1 + 1)) + 300,
-                            trainer: ["Aristide", "Michel", "Patrick"][
-                                Math.floor(Math.random() * 3)
-                            ],
-                        },
-                        {
-                            SKU: ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-                            (
-                                c ^
-                                (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-                                ).toString(16)
-                                ),
-                                name: ["GA4", "GTM", "BigQuery"][Math.floor(Math.random() * 3)],
-                                category: "Formations",
-                                price: Math.floor(Math.random() * (5000 - 1000 + 1)) + 300,
-                                duration: Math.floor(Math.random() * (5 - 1 + 1)) + 300,
-                                trainer: ["Aristide", "Michel", "Patrick"][
-                                    Math.floor(Math.random() * 3)
-                                ],
-                            },
-                        ],
-                    });
-                });
-                
-                
-                
-                
-                
-                
-                
-                var navTopItems = document.querySelectorAll('#nav_top li');
-                
-                navTopItems.forEach(function(item) {
-                    item.addEventListener('click', function(event) {
-                        event.preventDefault();
-                        
-                        var linkLabel = event.currentTarget.querySelector('a').textContent;
+            ),
+            name: ["GA4", "GTM", "BigQuery"][Math.floor(Math.random() * 3)],
+            category: "Formations",
+            price: Math.floor(Math.random() * (5000 - 1000 + 1)) + 300,
+            duration: Math.floor(Math.random() * (5 - 1 + 1)) + 300,
+            trainer: ["Aristide", "Michel", "Patrick"][
+                Math.floor(Math.random() * 3)
+            ],
+        },
+        {
+            SKU: ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+                (
+                c ^
+                (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+            ).toString(16)
+        ),
+        name: ["GA4", "GTM", "BigQuery"][Math.floor(Math.random() * 3)],
+        category: "Formations",
+        price: Math.floor(Math.random() * (5000 - 1000 + 1)) + 300,
+        duration: Math.floor(Math.random() * (5 - 1 + 1)) + 300,
+        trainer: ["Aristide", "Michel", "Patrick"][
+            Math.floor(Math.random() * 3)
+        ],
+    },
+],
+});
+});
 
-                        window.dataLayer = window.dataLayer || [];
 
-                        
-                        dataLayer.push({
-                            'event': 'navTopClick',
-                            'linkLabel': linkLabel
-                        });
-                    });
-                });
-                
-                
 
-                document.getElementById('demoForm').addEventListener('submit', function(event) {
-                    // event.preventDefault();
-                    const formData = new FormData(event.target);
-                    const queryString = new URLSearchParams(formData).toString();
-                    console.log('Form submitted with GET data:', queryString);
-                    // alert('Formulaire soumis !'); 
-                });
+
+
+
+
+var navTopItems = document.querySelectorAll('#nav_top li');
+
+navTopItems.forEach(function(item) {
+    item.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        var linkLabel = event.currentTarget.querySelector('a').textContent;
+        
+        window.dataLayer = window.dataLayer || [];
+        
+        
+        dataLayer.push({
+            'event': 'navTopClick',
+            'linkLabel': linkLabel
+        });
+    });
+});
+
+
+
+document.getElementById('demoForm').addEventListener('submit', function(event) {
+    // event.preventDefault();
+    const formData = new FormData(event.target);
+    const queryString = new URLSearchParams(formData).toString();
+    console.log('Form submitted with GET data:', queryString);
+    // alert('Formulaire soumis !'); 
+});
